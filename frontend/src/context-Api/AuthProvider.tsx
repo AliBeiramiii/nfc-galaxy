090f231cfe4bd266a139fbe8c92c86e637f0e622
Ali Beirami <alibeirami2003@gmail.com>
2024-02-09 19:13:40 +0100

    context API

--- frontend/src/context-Api/AuthProvider.tsx
+++ frontend/src/context-Api/AuthProvider.tsx
@@ -1,1 +1,24 @@
-const baseUrl='http://127.0.0.1:8000/api/'
+import React, { createContext, useState, ReactNode } from "react";
+
+interface AuthContextType {
+  auth: any; // Replace 'any' with the actual type of auth object
+  setAuth: React.Dispatch<React.SetStateAction<any>>; // Replace 'any' with the actual type of auth object
+}
+
+const AuthContext = createContext<AuthContextType>({ auth: {}, setAuth: () => {} });
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [auth, setAuth] = useState({});
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
